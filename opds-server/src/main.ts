479c1a98f838db00540530932dd759f045129187
pleroux <p.leroux@gmx.com>
2019-02-04 19:21:34 +0100

    up main for swagger and pipe

--- opds-server/src/main.ts
+++ opds-server/src/main.ts
@@ -13,9 +13,26 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from './pipe/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger Configuration
+  const options = new DocumentBuilder()
+    .setTitle('OPDS2 Server')
+    .setDescription('OPDS2 Webpub Manifest Server')
+    .setVersion('1.0')
+    .addTag('OPDS2')
+    .addBearerAuth('Authorization', 'header')
+    .build();
+    // .setBasePath(basePath)
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
