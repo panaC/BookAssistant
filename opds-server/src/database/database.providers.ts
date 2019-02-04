4f5e2a0547fe189be9cd9e2311cfdd41ca3e3007
pleroux <p.leroux@gmx.com>
2019-02-04 10:14:17 +0100

    add header in database providers and mongoose package

--- opds-server/src/database/database.providers.ts
+++ opds-server/src/database/database.providers.ts
@@ -1,3 +1,16 @@
+/*
+ * File: database.providers.ts
+ * Project: VoiceAssistant
+ * File Created: Monday, 4th February 2019 9:46:15 am
+ * Author: pierre (p.leroux@gmx.com)
+ * -----
+ * Last Modified: Monday, 4th February 2019 10:05:09 am
+ * Modified By: pierre (p.leroux@gmx.com>)
+ * -----
+ * Copyright 2019 - 2019 EDRLab.org
+ * Use of this source code is governed by a BSD-style license
+ */
+
 import * as mongoose from 'mongoose';
 import { DB_PROVIDER, DB_URI } from '../constants';
 
