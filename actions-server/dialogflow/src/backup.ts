acf95860f61f960d53eaad5d7004801271e0b11e
pleroux <p.leroux@gmx.com>
2019-02-07 16:36:29 +0100

    up backup add restore

--- actions-server/dialogflow/src/backup.ts
+++ actions-server/dialogflow/src/backup.ts
@@ -22,15 +22,18 @@ interface ICredential {
   private_key: string;
 }
 
-export class DiagflowAgent {
+const BACKUP_ZIP = 'backup.zip';
+
+export class DialogflowAgent {
   private client: dialogflow.AgentsClient;
 
-  constructor(keyFilename: string) {
+  constructor(keyFilename: string, private backupFilename: string = BACKUP_ZIP) {
     const credential: ICredential = JSON.parse(readFileSync(keyFilename).toString());
     this.client = new dialogflow.v2.AgentsClient({
       credentials: credential,
       projectId: credential.project_id,
     });
+    log('DialogflowAgent Constructor called');
   }
 
   public async backup() {
@@ -41,19 +44,47 @@ export class DiagflowAgent {
       });
 
       rep[0].on('complete', (result) => {
-        writeFileSync('backup.zip', Buffer.from(result.agentContent, 'base64'));
-        log('write backup.zip success');
+        writeFileSync(this.backupFilename, Buffer.from(result.agentContent, 'base64'));
+        log(`write ${this.backupFilename} success`);
       });
 
       rep[0].on('error', (e) => {
-        err(e);
+        throw e;
       });
     } catch (e) {
-      err('global', e);
+      throw new Error(e);
+    }
+  }
+
+  public async restore() {
+    try {
+      const name = await this.client.getProjectId();
+      const rep = await this.client.importAgent({
+        parent: this.client.projectPath(name),
+        agentContent: readFileSync(BACKUP_ZIP).toString('base64'),
+      });
+
+      rep[0].on('complete', () => {
+        log(`restore ${this.backupFilename} complete`);
+      });
+
+      rep[0].on('error', (e) => {
+        throw e;
+      });
+    } catch (e) {
+      throw new Error(e);
     }
   }
 }
 
 if (typeof require !== 'undefined' && require.main === module) {
-  new DiagflowAgent(JSON_CREDENTIAL_PATH).backup();
+  const l = new DialogflowAgent(JSON_CREDENTIAL_PATH);
+  (async () => {
+    try {
+      await l.backup();
+      await l.restore();
+    } catch (e) {
+      err(e);
+    }
+  })();
 }
