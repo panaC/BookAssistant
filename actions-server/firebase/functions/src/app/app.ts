1f019c559c91015cc15996457bec5f46f990bfde
pleroux <p.leroux@gmx.com>
2019-02-08 13:57:44 +0100

    up https edrlab server

--- actions-server/firebase/functions/src/app/app.ts
+++ actions-server/firebase/functions/src/app/app.ts
@@ -12,31 +12,41 @@
  */
 
 import { dialogflow, Image, MediaObject, Suggestions } from 'actions-on-google';
+import Axios from 'axios';
 
 // Create an app instance
-export const app = dialogflow();
+export const app = dialogflow({
+  debug: true,
+});
 
 // Register handlers for Dialogflow intents
 app.intent('Default Welcome Intent', conv => {
-  conv.ask(`Une musique Jazz :`);
-  /*conv.ask(new Image({
-    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-    alt: 'Un chat',
-  }));*/
+  conv.ask(`Que voulez-vous écouter ?`);
+});
+
+app.intent('play audiobook', async (conv, { audiobook }) => {
+  conv.ask(`Voici l'audiobook ${audiobook}`);
   if (!conv.surface.capabilities.has('actions.capability.MEDIA_RESPONSE_AUDIO')) {
     conv.ask('Désolé, cet appareil ne supporte pas la lecture audio');
     return;
   }
-  conv.ask(new MediaObject({
-    name: 'Jazz à Paris',
-    url: 'https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3#t=20',
-    description: 'Un morceau de Jazz',
-    icon: new Image({
-      url: 'https://storage.googleapis.com/automotive-media/album_art.jpg',
-      alt: 'Jazz musique',
-    }),
-  }));
-  conv.ask(new Suggestions('fermée'));
+  try {
+    const res = await Axios.get(`https://edrlab.ml/api?q=${encodeURI(audiobook as string)}`);
+    if (res.data.links[0].href) {
+      conv.ask(new MediaObject({
+        name: res.data.metadata.title,
+        url: res.data.links[0].href,
+        description: res.data.metadata.identifier,
+        icon: new Image({
+          url: 'https://storage.googleapis.com/automotive-media/album_art.jpg',
+          alt: 'Jazz musique',
+        }),
+      }));
+      conv.ask(new Suggestions('Ma suggestion'));
+    }
+  } catch (e) {
+    conv.ask(`Une érreur est survenue : ${e}`);
+  }
 });
 
 // Intent in Dialogflow called `Goodbye`
@@ -45,5 +55,5 @@ app.intent('Goodbye', conv => {
 });
 
 app.intent('Default Fallback Intent', conv => {
-  conv.ask(`Je n'ai pas compris peux tu me demandé autre chose ?`);
+  conv.ask(`Je n'ai pas compris peux tu me demander autre chose ?`);
 });
