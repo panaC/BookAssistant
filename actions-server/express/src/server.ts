969fc826bf26288f55e86c78b159e85ded0650e8
pleroux <p.leroux@gmx.com>
2019-02-25 15:36:48 +0100

    test v2 switch

--- actions-server/express/src/server.ts
+++ actions-server/express/src/server.ts
@@ -20,12 +20,12 @@ export const server = express();
 
 server.set('port', PORT);
 server.set('trust proxy', 'loopback');
-server.use(bodyParser.json());
-server.use((req, res, next) => {
+server.use(bodyParser.json({type: 'application/json'}));
+/*server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});
+});*/
 server.post(ENTRY_POINT, app);
 server.get('/', (req, res) => {
   res.send(SERVER_NAME);
