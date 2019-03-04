53c88c19c3755939e938590abf0bb620fdce85d2
pleroux <p.leroux@gmx.com>
2019-03-04 09:34:08 +0100

    add domain extern for auth-server and test it

--- auth-server/src/constants.ts
+++ auth-server/src/constants.ts
@@ -1,4 +1,4 @@
 export const NAME_SERVER = 'auth-server';
 export const PORT_SERVER = 5000;
-export const DOMAIN_SERVER = 'localhost';
-export const PROTOCOL_SERVER = 'http';
+export const DOMAIN_SERVER = 'edrlab.ml';
+export const PROTOCOL_SERVER = 'https';
