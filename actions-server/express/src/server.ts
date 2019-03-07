86a9f94202f948294ec6910755eb1b4491bc86c2
pleroux <p.leroux@gmx.com>
2019-03-07 17:16:03 +0100

    clean cors unused

--- actions-server/express/src/server.ts
+++ actions-server/express/src/server.ts
@@ -21,11 +21,6 @@ export const server = express();
 server.set('port', PORT);
 server.set('trust proxy', 'loopback');
 server.use(express.json({}));
-/*server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});*/
 server.post(ENTRY_POINT, app);
 server.get('/', (req, res) => {
   res.send(SERVER_NAME);
