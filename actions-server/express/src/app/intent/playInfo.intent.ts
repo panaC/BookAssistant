345f2b71ac9ee92c5fd6d0332a82f5bb96e8578d
pleroux <p.leroux@gmx.com>
2019-03-07 14:16:47 +0100

    fix forgot add +1 to display chapter

--- actions-server/express/src/app/intent/playInfo.intent.ts
+++ actions-server/express/src/app/intent/playInfo.intent.ts
@@ -3,12 +3,12 @@ import { prompts } from '../prompt';
 
 export const numberOfChapter = (conv: DFConv) => {
   conv.utils.ask(prompts.play_numberOfChapter
-    , conv.data.currentPlayingMedia.numberOfChapter);
+    , conv.data.currentPlayingMedia.numberOfChapter + 1);
 };
 
 export const currentChapter = (conv: DFConv) => {
   conv.utils.ask(prompts.play_chapter
-    , conv.data.currentPlayingMedia.chapter);
+    , conv.data.currentPlayingMedia.chapter + 1);
 }
 
 export const author = (conv: DFConv) => {
