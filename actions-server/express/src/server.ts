d43c946a7f29e58b1477ab224f0119859db17145
pleroux <p.leroux@gmx.com>
2019-03-07 17:10:10 +0100

    test bodyparser in middleware express

--- actions-server/express/src/server.ts
+++ actions-server/express/src/server.ts
@@ -20,7 +20,7 @@ export const server = express();
 
 server.set('port', PORT);
 server.set('trust proxy', 'loopback');
-server.use(bodyParser.json({type: 'application/json'}));
+server.use(express.json({}));
 /*server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
