a7d626a7fdba8b2295ede5c877570a41fb088034
Pierre Leroux <p.leroux@gmx.com>
2019-03-11 11:09:03 +0100

    Create firewall.sh

---
+++ opds/script/firewall.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+##
+# see https://documentation.online.net/fr/dedicated-server/tutorials/security/configure-firewall
+##
+
+sudo apt-get install ufw -y
+
+# Par défaut, on refuse tout.
+sudo ufw default deny
+# Puis, nous autorisons le trafic sortant
+sudo ufw default allow outgoing
+
+# ssh 22
+sudo ufw allow 22/tcp
+# https 443
+sudo ufw allow 443/tcp
+
+# enable firewall
+sudo ufw enable
+
+echo "firewall enabled"
+sudo ufw status numbered
