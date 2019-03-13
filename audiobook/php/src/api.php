8481a354cd646fd477ec030fa3d74accd5eeb71a
pleroux <p.leroux@gmx.com>
2019-03-13 18:25:55 +0100

    php script for looking into audiobook path. will be used for audiobook publishing form. This script will be behind /audiobook nginx route

---
+++ audiobook/php/src/api.php
@@ -0,0 +1,26 @@
+<?php
+
+$audiobookPath    = '/home/pierre/Documents/audiobook/';
+$path = $audiobookPath;
+
+if(isset($_GET["path"])) {
+    $userPath   = htmlspecialchars($_GET["path"]);
+    preg_match('/.*(\.\.).*/', $userPath, $output_array);
+    if (!$output_array[1]) {
+      $path = $audiobookPath . $userPath;
+    }
+}
+
+$json->path = true;
+$json->files = scandir($path);
+
+if (!$json->files) {
+  $json->path = false;
+  $json->files = [];
+} else {
+  array_splice($json->files, 0, 2);
+}
+
+header('Content-Type: application/json');
+echo json_encode($json);
+?>
