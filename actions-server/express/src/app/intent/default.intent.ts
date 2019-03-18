a620b629d1fd40d33bca4ac4d7d86bbf20d62916
pleroux <p.leroux@gmx.com>
2019-03-18 10:41:11 +0100

    test no-input reprompt

--- actions-server/express/src/app/intent/default.intent.ts
+++ actions-server/express/src/app/intent/default.intent.ts
@@ -2,7 +2,15 @@ import { DFConv } from './../app';
 import { prompts } from '../prompt';
 
 export const noInput = (conv: DFConv) => {
-  conv.utils.ask(prompts.default_noInput);
+  const repromptCount = parseInt(conv.arguments.get('REPROMPT_COUNT'));
+  if (repromptCount === 0) {
+  conv.ask(`1er essai`);
+  } else if (repromptCount === 1) {
+  conv.ask(`2eme essai`);
+  } else if (conv.arguments.get('IS_FINAL_REPROMPT')) {
+  conv.ask(`3eme essai`);
+  }
+  //conv.utils.ask(prompts.default_noInput);
 };
 
 export const goodbye = (conv: DFConv) => {
