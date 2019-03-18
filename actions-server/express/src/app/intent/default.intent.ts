a744c5ad606589a6b116b164e1dff1f3c1563b8b
pleroux <p.leroux@gmx.com>
2019-03-18 11:01:54 +0100

    changed no-input fr text

--- actions-server/express/src/app/intent/default.intent.ts
+++ actions-server/express/src/app/intent/default.intent.ts
@@ -4,13 +4,13 @@ import { prompts } from '../prompt';
 export const noInput = (conv: DFConv) => {
   const repromptCount = parseInt(conv.arguments.get('REPROMPT_COUNT'));
   if (repromptCount === 0) {
-  conv.ask(`1er essai`);
+    conv.utils.ask(prompts.default_noInput);
   } else if (repromptCount === 1) {
-  conv.ask(`2eme essai`);
+    conv.utils.ask(prompts.default_noInput);
   } else if (conv.arguments.get('IS_FINAL_REPROMPT')) {
-  conv.ask(`3eme essai`);
+    // doesn't work ! Didn't pass here because catch by Native Assistant before
+    conv.utils.ask(prompts.default_noInput);
   }
-  //conv.utils.ask(prompts.default_noInput);
 };
 
 export const goodbye = (conv: DFConv) => {
