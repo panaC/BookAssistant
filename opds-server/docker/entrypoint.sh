53e7758c372c1f9053965e30d1d7c78b07dea80a
pleroux <p.leroux@gmx.com>
2019-03-21 17:01:40 +0100

    update git update dev branch

--- opds-server/docker/entrypoint.sh
+++ opds-server/docker/entrypoint.sh
@@ -4,14 +4,13 @@ set -e
 
 if [ "$NODE_ENV" = "development" ] ; then
 
-  mkdir /var/log/bookAssistant
   git clone https://github.com/panaC/BookAssistant.git /app-dev
   cd /app-dev/opds-server
   git checkout opds-server
   while true; do 
     sleep 10
-    git fetch --all >> /var/log/bookAssistant/gitlog.txt
-    git reset --hard origin/opds-server >> /var/log/bookAssistant/gitlog.txt
+    git stash
+    git pull origin ops-server
   done &
   npm install
   npm run start:dev
