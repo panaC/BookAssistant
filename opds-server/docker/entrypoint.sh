9c4b2900778ae3506b6059316d4620a35cb43a27
pleroux <p.leroux@gmx.com>
2019-03-22 10:29:02 +0100

    up entrypoint in opds-server with the same auto-check that actions

--- opds-server/docker/entrypoint.sh
+++ opds-server/docker/entrypoint.sh
@@ -7,12 +7,14 @@ if [ "$NODE_ENV" = "development" ] ; then
   git clone https://github.com/panaC/BookAssistant.git /app-dev
   cd /app-dev/opds-server
   git checkout opds-server
-  while true; do 
-    sleep 10
-    git stash
-    git pull origin ops-server
-  done &
   npm install
+  while true; do 
+    # auto uptade file in the right branch
+    sleep 10
+    # https://tomkadwill.com/git-reset-or-git-pull
+    git fetch origin opds-server
+    git reset --hard origin/opds-server
+  done &
   npm run start:dev
   exit 1
 fi
