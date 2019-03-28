6b9acd002c8c05c9d7647134e2c67016274db37e
pleroux <p.leroux@gmx.com>
2019-03-28 14:45:57 +0100

    beautify media.service

--- actions-server/express/src/app/service/media.service.ts
+++ actions-server/express/src/app/service/media.service.ts
@@ -1,3 +1,16 @@
+/*
+ * File: media.service.ts
+ * Project: VoiceAssistant
+ * File Created: Thursday, 28th March 2019 2:44:31 pm
+ * Author: pierre (p.leroux@gmx.com)
+ * -----
+ * Last Modified: Thursday, 28th March 2019 2:44:53 pm
+ * Modified By: pierre (p.leroux@gmx.com>)
+ * -----
+ * Copyright 2019 - 2019 EDRLab.org
+ * Use of this source code is governed by a BSD-style license
+ */
+
 import { SEARCH } from '../../constants';
 import Axios from 'axios';
 import { ILinks } from '../../../../../opds-server/src/webpub/interfaces/links.interface';
@@ -15,13 +28,18 @@ export const getLink = (a: IWebpub) => a.links.filter((ln) => ln.rel === 'audiob
 
 export const getImg = (a: IWebpub) => a.resources.filter((ln) => ln.rel === 'cover').pop();
 
+// Middleware Media
+// request from app.middleware
+// Handle all the media provider
+// set the title and author of book
+// get and save to session the playing media
 export class MediaService {
   constructor(private _conv: DFConv) {
   }
 
   private _title: string = undefined;
   private _author: string = undefined;
-  private _webpub: IWebpub[];
+  private _webpub: IWebpub[] = [];
   private _nb: number = 0;
   private _length: number = 0;
   private _state: Eaudiobook = Eaudiobook.OK;
@@ -32,12 +50,18 @@ export class MediaService {
 
   set title(t: string) {
     this._title = t;
+    this._webpub = [];
+    this._nb = 0;
   }
 
   set author(a: string) {
     this._author = a;
+    this._webpub = [];
+    this._nb = 0;
   }
 
+  // first step in "I want listen" intent
+  // get the length of array from api webpub
   public async length() {
     await this.getWebpub();
     if (this._state === Eaudiobook.OK) {
@@ -46,17 +70,21 @@ export class MediaService {
     return 0;
   }
 
-  public async choice(nb: number) {
+  // second step
+  // get and save the selected wepbub
+  public async choice(nb: number = 0) {
     if (this._length > nb && nb >= 0) {
       this._nb = nb;
-      await this.getWebpub();
-      if (this._state == Eaudiobook.OK) {
-        this._conv.session.state.currentPlayingMedia = this.media();
-        this._conv.session.save();
+      if (!this._webpub.length) {
+        await this.getWebpub();
       }
+      this._conv.session.state.currentPlayingMedia = this.media();
+      this._conv.session.save();
     }
   }
 
+  // private method for handle media in Iplaying Format
+  // generate a play media
   private media() {
     let a = this.webpub;
     let chapter = this._conv.session.state.chapterToPlay;
@@ -97,6 +125,8 @@ export class MediaService {
     return getState(this._state);
   }
 
+  // private method
+  // get webpub[] by http request to API webpub
   private async getWebpub(title: string = undefined, author: string = undefined) {
     const res = await Axios.get<IWebpub[]>(SEARCH(title || this._title, author || this._author));
     if (res && res.data && !res.data.length) {
@@ -107,6 +137,5 @@ export class MediaService {
     this._state = Eaudiobook.OK;
     this._webpub = res.data;
   }
-
 }
 
