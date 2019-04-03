c09153c341ac1347695c76833c8c3e5263bd9e11
pleroux <p.leroux@gmx.com>
2019-04-03 16:18:27 +0200

    add new state schema interface, i'm thinking

--- actions-server/express/src/interface/state.interface.ts
+++ actions-server/express/src/interface/state.interface.ts
@@ -13,24 +13,68 @@
 
 export interface IstateName {
   context?: string | string[];
+
+  // declare var used
+  var?: Ivar;
+
+  // http request requisite
+  http?: Ihttp | Ihttp[];
+
+  // variable to compute
+  compute?: string | string[];
+
   // handle multiple string for apply a map-reduce logic in fct -> return a concatenation string
   // accept in fct : variable name or function name, type test before execution
-  fct?: string | string[];
+  test?: string | string[];
   // only default switch state is required
   switch: {
+    // Option :
     // allow to add multiple context name with string[]
-    [result: string]: string | string[];
+    [result: string]: string /*| string[]*/;
     default: string;
-  }
+  };
   conv?: {
     arg?: string | string[];
     ask?: string | string[];
     close?: string;
+    media?: Imedia;
     suggestion?: string | string[];
-  }
+  };
   // no by default
   return?: boolean;
   children?: IstateChildren;
+}
+
+export interface Imedia {
+  name: string;
+  url: string;
+  description: string;
+  author: string;
+  img: {
+    url: string;
+    alt: string;
+  };
+}
+
+export interface Ihttp {
+  url: string;
+  type: string;
+  body: string;
+  header: string;
+  save: {
+    [name: string]: string;
+  };
+}
+
+export interface Ivar {
+  [name: string]: {
+    // false by default
+    userInfo: boolean;
+    session: boolean;
+    array: boolean;
+    type: string;
+    init: string;
+  };
 }
 
 export interface IstateChildren {
