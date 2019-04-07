1ff3c67cd592907d937a23886b742b2e5c5b414d
pleroux <p.leroux@gmx.com>
2019-04-07 11:57:33 +0200

    fix search constant

--- actions-server/express/src/constants.ts
+++ actions-server/express/src/constants.ts
@@ -20,8 +20,8 @@ export const ENTRY_POINT = '/fulfillment';
 export const SERVER_URL = process.env.OPDS_SERVER_URL || 'http://127.0.0.1:3000/webpub';
 export const DB_URL = process.env.COUCHDB_URL || 'http://127.0.0.1:5984';
 
-export const SEARCH = (title: string, author: string = undefined) => 
-  `${SERVER_URL}?q=title:${encodeURI(title)}${author ? ` AND author:${author}` : ''}`;
+export const SEARCH = (title: string, author: string = '') => 
+  `${SERVER_URL}?q=title:${encodeURI(title)}${author !== '' ? ` AND author:${author}` : ''}`;
 export const REF = (identifier: string, ref: string) => `${SERVER_URL}/${identifier}?q=${encodeURI(ref)}`;
 
 export const MAE_LOOP_MAX = 100;
