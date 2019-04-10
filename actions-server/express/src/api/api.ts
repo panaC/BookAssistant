0f4490d1bec47c7c7fc288aa778cbc3c519c1cef
pleroux <p.leroux@gmx.com>
2019-04-10 16:56:18 +0200

    thinking on api request middleware

---
+++ actions-server/express/src/api/api.ts
@@ -0,0 +1,25 @@
+
+/**
+ * api http request middleware
+ * 
+ * - the api can be webpub manifest
+ * - or Daisy 2.02
+ * - or Epub3
+ * - ...
+ * 
+ * handle 
+ * 
+ * interface with keyof of this interface in node.interface
+ * how do implement this ?
+ * 
+ * in clear doing an interface api implement in node
+ * 
+ * 
+ * in the api allow multiple interface type 
+ * for each route
+ * like :
+ * - discovery catalog route
+ * - search book route
+ * - whatever
+ * 
+ */
