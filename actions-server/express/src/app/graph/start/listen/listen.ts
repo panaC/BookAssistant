707a5329d3154fba9fdb355a7639876fdfe649ac
pleroux <p.leroux@gmx.com>
2019-04-24 18:17:45 +0200

    up listen context

--- actions-server/express/src/app/graph/start/listen/listen.ts
+++ actions-server/express/src/app/graph/start/listen/listen.ts
@@ -5,8 +5,12 @@ export const start: Inode = {
   intent: true,
   name: 'start.listen_intent',
   return: false,
+  /*
+  // listen context is actually doesn't used
+  // the alone return node is play.play
   context: {
     name: 'listen',
   },
+  */
   test: () => 'listen.getBook'
 };
