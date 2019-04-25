f871d320df92c5a553026f0640c159b26fb82522
pleroux <p.leroux@gmx.com>
2019-04-25 18:08:57 +0200

    test new search api

--- actions-server/express/src/app/api/src/search.api.ts
+++ actions-server/express/src/app/api/src/search.api.ts
@@ -5,11 +5,15 @@ import {debug} from './../../../core/utils/debug';
 
 export const search = async (conv: IDFConv, title: string, author?: string) => {
   try {
-    conv.middleware.db.session.api.search =
+    if (author) {
+      conv.middleware.db.session.api.search =
         await conv.middleware.get<Iwebpub[]>(
-            `https://edrlab.tk/api?q=title:${encodeURI(title)}${
-                author !== undefined ? ` AND author:${encodeURI(author)}` :
-                                       ''}`);
+            `https://edrlab.tk/api?q=title:${encodeURI(title)} AND author:${encodeURI(author)}`);
+    } else {
+      conv.middleware.db.session.api.search =
+        await conv.middleware.get<Iwebpub[]>(
+            `https://edrlab.tk/api?q=${encodeURI(title)}`);
+    }
   } catch (e) {
     debug.service.log('search', e);
   }
