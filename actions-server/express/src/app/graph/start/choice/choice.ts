cd56f730c71c78d818f8737e81f4d581b1618087
pleroux <p.leroux@gmx.com>
2019-04-25 16:30:32 +0200

    debug choice

--- actions-server/express/src/app/graph/start/choice/choice.ts
+++ actions-server/express/src/app/graph/start/choice/choice.ts
@@ -1,3 +1,4 @@
+import { debug } from './../../../../core/utils/debug';
 import { Inode } from './../../../../core/middleware/graph';
 import { InodeTable } from '../../../interface';
 
@@ -8,6 +9,7 @@ export const getNumberIntent: Inode = {
   test: (conv) => {
     // tslint:disable-next-line:ban
     conv.middleware.db.session.data.context.choice = parseInt(conv.parameters.choice as string, 10);
+    debug.app.log('getNumber choice:', conv.middleware.db.session.data.context.choice);
     const param = conv.contexts.get('choice');
     if (param) {
       // add check if param return is not corrupted
